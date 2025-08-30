404d90b9f676d006a2000efa89d9bdaf52bb94e5
Brendon Raw <brendon.raw@ane.na>
2025-08-30 18:25:16 +0200

    Added popular courses chart

---
+++ ViewModels/CourseStudentCount.cs
@@ -0,0 +1,8 @@
+﻿namespace CourseRegisterApp.ViewModels
+{
+    public class CourseStudentCount
+    {
+        public string CourseName { get; set; }
+        public int StudentCount { get; set; }
+    }
+}
